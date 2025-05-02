a1aff826ba1cf2be5c64257c30cd8e5bade36e92
jpberse <jp.bersezio@gmail.com>
2025-05-02 13:30:23 -0400

    Añade persistencia con Zustand usando localStorage

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand'
 import { mockTodos } from '../mock/mockTodos'
 import { type filterValue, type ListOfTodos, type todoCompleted, type todoId, type todoTitle } from '../types'
 import { TODO_FILTERS } from '../consts'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 interface TodoState {
     todos: ListOfTodos
@@ -13,44 +14,50 @@ interface TodoState {
     handleClearAllcompleted: () => void
 }
 
-export const useTodoStore = create<TodoState>()(((set, get) => ({
-      todos: mockTodos,
-      filter: TODO_FILTERS.All,
-      handleRemove: (id) => {
-        const { todos } = get()
-        const newTodos = todos.filter(todo => todo.id !== id)
-        set({ todos: newTodos})
-      },
-      handleComplete: ({id, completed}) => {
-        const { todos } = get()
-        const newTodos = todos.map(todo => {
-          if (todo.id === id) {
-            return {
-              ...todo,
-              completed
-            }
+export const useTodoStore = create<TodoState>()(
+  persist(((set, get) => ({
+    todos: [],
+    filter: TODO_FILTERS.All,
+    handleRemove: (id) => {
+      const { todos } = get()
+      const newTodos = todos.filter(todo => todo.id !== id)
+      set({ todos: newTodos})
+    },
+    handleComplete: ({id, completed}) => {
+      const { todos } = get()
+      const newTodos = todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed
           }
-          return todo
-        })
-        set({ todos: newTodos})
-      },
-      handleAddTodo: (title) => {
-        const { todos } = get()
-        const newTodo = {
-          title,
-          id: crypto.randomUUID(),
-          completed: false
         }
-        const newTodos = [...todos, newTodo]
-        set({ todos: newTodos})
-      },
-      handleFilterChange: (filterChange) => {
-        set({ filter: filterChange })
-      },
-      handleClearAllcompleted: () => {
-        const { todos } = get()
-        const newTodos = todos.filter(todo => !todo.completed)
-        set({ todos: newTodos})
+        return todo
+      })
+      set({ todos: newTodos})
+    },
+    handleAddTodo: (title) => {
+      const { todos } = get()
+      const newTodo = {
+        title,
+        id: crypto.randomUUID(),
+        completed: false
       }
-    }))
-  );
+      const newTodos = [...todos, newTodo]
+      set({ todos: newTodos})
+    },
+    handleFilterChange: (filterChange) => {
+      set({ filter: filterChange })
+    },
+    handleClearAllcompleted: () => {
+      const { todos } = get()
+      const newTodos = todos.filter(todo => !todo.completed)
+      set({ todos: newTodos})
+    }
+  })),
+  {
+    name: 'todo-storage',
+    storage: createJSONStorage(() => localStorage),
+    partialize: (state) => ({ todos: state.todos, filter: state.filter})
+  }
+  ));
